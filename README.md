7478ead6f2e4ed8be4b29f9554243ed9d8a8c9f3
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2020-08-30 22:45:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SPBR_Depth_Peeling
---
+++ os_select.h
@@ -0,0 +1,16 @@
+///////////////////////
+///// os_select.h /////
+///////////////////////
+
+#if !defined  OS_SELECT_HH
+#define       OS_SELECT_HH
+
+
+//*** Select one from "OS_WIN" and "OS_UNIX"
+#define OS_UNIX
+//#define OS_WIN
+
+
+#endif
+//end of os_select.h
+
