1a30b0d5220c2bae9dae07e84569544e11d8b3db
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2020-10-26 04:20:23 +0900

    Updated

--- src/py/layer_image_averaging/customised_layer_image_averaging.py
+++ src/py/layer_image_averaging/customised_layer_image_averaging.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import cv2
+import time
 
 # Check arguments
 import sys
@@ -13,66 +14,99 @@ if len(args) != 4:
     print("EXAMPLE : $ python {} ../IMAGE_DATA 10 1000\n".format(args[0]))
     sys.exit()
 
-def create_empty_images( _num_of_images ):
-    empty_images_R = np.empty( (image_resol*1, image_resol*1, _num_of_images), dtype=np.uint8 )
-    empty_images_G = np.empty( (image_resol*1, image_resol*1, _num_of_images), dtype=np.uint8 )
-    empty_images_B = np.empty( (image_resol*1, image_resol*1, _num_of_images), dtype=np.uint8 )
+def create_empty_2d_array():
+    empty_2d_array = np.empty( ( image_resol, image_resol ), dtype=np.uint8 )
 
-    return empty_images_R, empty_images_G, empty_images_B
+    return empty_2d_array
+
+def create_empty_3d_array( _num_of_images ):
+    empty_3d_array = np.empty( ( image_resol, image_resol, _num_of_images ), dtype=np.uint8 )
+    
+    return empty_3d_array
 
 def read_layer_images():
     # Read the layer images
-    for i in range( num_of_layers ):
+    for layer in range( num_of_layers ):
         # Read each layer image
-        tmp_image_BGR = cv2.imread( layer_images_path + "LayerImage" + str( i + 1 ) + ".bmp" )
-        tmp_image_RGB = cv2.cvtColor( tmp_image_BGR, cv2.COLOR_BGR2RGB )
+        tmp_image_BGR  = cv2.imread( layer_images_path + "LayerImage" + str( layer + 1 ) + ".bmp" )
+        tmp_image_RGB  = cv2.cvtColor( tmp_image_BGR, cv2.COLOR_BGR2RGB )
+        tmp_image_GRAY = cv2.cvtColor( tmp_image_RGB, cv2.COLOR_RGB2GRAY )
+        
+        # Add to numpy array
+        layer_images_R[:,:,layer]    = tmp_image_RGB[:,:,0] # R
+        layer_images_G[:,:,layer]    = tmp_image_RGB[:,:,1] # G
+        layer_images_B[:,:,layer]    = tmp_image_RGB[:,:,2] # B
+        layer_images_GRAY[:,:,layer] = tmp_image_GRAY       # GRAY
 
-        # Split into RGB and add to numpy array
-        layer_images_R[:,:,i] = tmp_image_RGB[:,:,0] # R
-        layer_images_G[:,:,i] = tmp_image_RGB[:,:,1] # G
-        layer_images_B[:,:,i] = tmp_image_RGB[:,:,2] # B
+        if layer == 0:
+            print( "R   : {}".format( layer_images_R.shape ) )
+            print( "G   : {}".format( layer_images_G.shape ) )
+            print( "B   : {}".format( layer_images_B.shape ) )
+            print( "GRAY: {}".format( layer_images_GRAY.shape ) )
+    # end for layer
+# End read_layer_images()
 
-        if i == num_of_layers-1:
-            print( "R: {}".format( layer_images_R.shape ) )
-            print( "G: {}".format( layer_images_G.shape ) )
-            print( "B: {}".format( layer_images_B.shape ) )
-    # end for
+def is_this_pixel_noise( _target_pixel_value ):
+    is_noise_pixel = False
+    return is_noise_pixel
 
 def average_layer_images():
     # Create median image
-    median_image_R, median_image_G, median_image_B = create_empty_images( 1 )
+    median_image_R = create_empty_2d_array()
+    median_image_G = create_empty_2d_array()
+    median_image_B = create_empty_2d_array()
     median_image_R = np.median( layer_images_R, axis=2 )
     median_image_G = np.median( layer_images_G, axis=2 )
     median_image_B = np.median( layer_images_B, axis=2 )
-    
-    # Pixelwise layer image averaging
-    # M_array = np.empty( (image_resol*1, image_resol*1), float )
-    for y in range( image_resol ):     # height
-        for x in range( image_resol ): # width
-            for l in range( num_of_layers ):
-                layer_images_R[y,x,l]
-                layer_images_G[y,x,l]
-                layer_images_B[y,x,l]
 
-                final_pixel_value = 0
-            # end for l
+    # Pixelwise layer image averaging
+    layer_averaged_image = create_empty_2d_array()
+    bool_idx = np.empty( (num_of_layers), bool )
+    print( "\nNow calculating..." )
+    start = time.time()
+    for y in range( image_resol ):
+        for x in range( image_resol ):
+            for layer in range( num_of_layers ):
+                # Get the target pixel value
+                target_pixel_value_GRAY = layer_images_GRAY[y,x,layer]
+
+                # Check if the pixel is noise pixel
+                if is_this_pixel_noise( target_pixel_value_GRAY ) == False:
+                    bool_idx[layer] = True
+                else:
+                    bool_idx[layer] = False
+                # end if
+            # end for layer
+
+            # Extract only non-noise pixels
+            non_noise_pixel_values = layer_images_GRAY[y,x,bool_idx]
+
+            # Average the pixel values
+            layer_averaged_image[y,x] = np.sum( non_noise_pixel_values ) / non_noise_pixel_values.size
         # end for x
     # end for y
 
+    print ("Done! ({} [sec])\n".format( round(time.time() - start , 2) ) )
+
+    # Save the result image
+    # layer_averaged_image_BGR = cv2.cvtColor( layer_averaged_image, cv2.COLOR_RGB2BGR )
+    cv2.imwrite( layer_images_path + "Layer_Averaged_Image.png", layer_averaged_image )
 
 if __name__ == "__main__":
     # Set the number of layer images
     num_of_layers = int( args[2] )
-    print( "Number of Layers: {}".format( num_of_layers ) )
+    print( "\nNumber of Layers: {}".format( num_of_layers ) )
 
     # Set image resolution
     image_resol = int( args[3] )
     print( "Image Resolution: {}".format( image_resol ) )
 
-
     # Read the target layer images
+    layer_images_R    = create_empty_3d_array( num_of_layers )
+    layer_images_G    = create_empty_3d_array( num_of_layers )
+    layer_images_B    = create_empty_3d_array( num_of_layers )
+    layer_images_GRAY = create_empty_3d_array( num_of_layers )
     layer_images_path = args[1] + "/"
-    layer_images_R, layer_images_G, layer_images_B = create_empty_images( num_of_layers )
     read_layer_images()
 
     # Average the layer images
